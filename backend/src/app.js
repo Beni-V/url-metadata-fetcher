80ac3cbb51e4fff16b966151051202f891e352f3
Ben <binitto10@gmail.com>
2024-08-17 17:32:46 +0300

    again

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const helmet = require('helmet');
+const path = require('path');
 const metadataController = require('./controllers/metadataController');
 const rateLimiter = require('./utils/rateLimiter');
 
@@ -12,5 +13,19 @@ app.use(rateLimiter);
 app.set('trust proxy', 1)
 
 app.post('/fetch-metadata', metadataController.fetchMetadata);
+
+const PORT = process.env.PORT || 80;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Catch-all route to serve the frontend app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
